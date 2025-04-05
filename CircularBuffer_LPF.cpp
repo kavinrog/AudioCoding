08d0394c1766a9c42a9e87de8044e3e6ba278945
kavinrog <kavinderroghit.k@gmail.com>
2025-04-05 14:48:06 -0700

    Add LPF Class

--- CircularBuffer_LPF.cpp
+++ CircularBuffer_LPF.cpp
@@ -6,14 +6,26 @@ class CircularBuffer{
     std::vector<float> buffer;
     int size, writeIndex = 0;
 public: 
-    CircularBuffer(int s) : size(s), buffer(s, 0.0f){}
+    CircularBuffer(int s): size(s), buffer(s, 0.0f){}
     void write(float sample){
         buffer[writeIndex] = sample;
         writeIndex = (writeIndex+1)%size;
     }
     float read(int delay) const{
-        int index = (writeIndex-delay +size)%size;
+        int index = (writeIndex - delay +size)%size;
         return buffer[index];
+    }
+};
 
+template<typename T>
+class LowPassFilter{
+    T alpha;
+    T prevOutput;
+public: 
+    LowPassFilter(T alphaVal) : alpha(alphaVal){}
+    T process(T input){
+        T output = alpha * input * (1-alpha) * prevOutput;
+        prevOutput = output
+        return output
     }
 };
