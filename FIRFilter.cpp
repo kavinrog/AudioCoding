46793f5ead2fdee55c952d2ead502f95e31a6fbe
kavinrog <kavinderroghit.k@gmail.com>
2025-05-10 22:46:10 -0700

    Add code

--- FIRFilter.cpp
+++ FIRFilter.cpp
@@ -1,19 +1,29 @@
 #include <vector>
-#include <deque>
-class FIRFilter{
-    std::vector<float> coeffs;
-    std::deque<float> buffer;
-public:
-    FIRFilter(const std::vector<float>& coefficients) :coeffs(coefficients)
-, buffer(coefficients.size(), 0.0f){}
-    float processSample(float inputSample){
-        buffer.pop_back();
-        buffer.push_front(inputSample);
-        float result = 0.0f;
-        for (size_t i = 0; i<coeffs.size(); ++i){
-            result+=coeffs[i]* buffer[i];
-        }
-        return result ;
-    }
-};
 
+class FIRFilter {
+public:
+    FIRFilter(const std::vector<float>& taps)
+        : coefficients(taps), buffer(taps.size(), 0.0f), index(0){}
+
+        float process(float input){
+            buffer[index] = input;
+
+            float output =0.0f;
+            size_t tapIndex = 0;
+            size_t bufIndex = index;
+            for(; tapIndex<coefficients.size(); ++tapIndex){
+                output += coefficients[tapIndex] * buffer[bufIndex];
+                if(bufIndex == 0)
+                    bufIndex = buffer.size()-1;
+                else
+                    --bufIndex;
+            }
+            index = (index +1) % buffer.size();
+            return output;
+
+        }
+private:
+    std::vector<float> coefficients;
+    std::vector<float> buffer;
+    size_t index;
+};
